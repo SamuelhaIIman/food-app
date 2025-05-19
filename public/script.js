5bed7c1c470d5b63feeaa460fc97d442bf83379a
Samuel Hållman <samuel.hollman@hotmail.com>
2025-05-19 15:28:49 +0300

    Tämän päivän aikana sain tehtyä hyvin yksinkertaisen laskurin minun sovellukselle. Aloitin myös tekemään osiota mistä voi lisätä tuotteita ostoskoriin. Sain tehtyä yksinkertaisen alun tälle toiminnolle.

--- public/script.js
+++ public/script.js
@@ -1,6 +1,14 @@
-const slider = document.getElementById("slider");
-const output = document.getElementById("demo");
+
+const slider = document.getElementById("budget-slider");
+const output = document.getElementById("budget-money-amount");
+const budget = document.getElementById("budget-money-per-day");
 
 slider.addEventListener("input", () => {
     output.value = slider.value;
+    if(output.value > 500 ) {
+        output.value = 500;
+    }
+    
+    let budgetDayCalculation = output.value / 30;
+    budget.innerHTML = budgetDayCalculation.toFixed(2);
 });
