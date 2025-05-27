510aea94314cded9f5b8f7c5f388f612982e891e
Samuel Hållman <samuel.hollman@hotmail.com>
2025-05-27 12:33:53 +0300

    Succesful test with file I created a text file and saved the information on it by using fs. My aim is to have it saved in a db in the future. The prices and the products names also. This was a great success!

--- index.js
+++ index.js
@@ -19,17 +19,17 @@ async function getPrice() {
 }
 getPrice();
 
+const filePath = "pricesFile.txt";
 const meatPrices = [];
 async function getAllPrices() {
     try {
         const response = await axios.get(url, {
             headers: {
-                "User-Agent": "Mozilla/5.0", // Spoof user-agent
+                "User-Agent": "Mozilla/5.0",
             },
         });
         const $ = cheerio.load(response.data);
 
-        // Use data-test-id for a stable selector
         const prices = $('[data-test-id="display-price"]');
 
         prices.each(function () {
@@ -39,7 +39,19 @@ async function getAllPrices() {
             }
         });
 
-        console.log(meatPrices);
+        if (fs.existsSync(filePath)) {
+            fs.writeFile(filePath, JSON.stringify(meatPrices) + "\n", (err) => {
+                if (err) throw err;
+                console.log("File found!");
+            });
+        } else {
+            fs.appendFile(filePath, JSON.stringify(meatPrices) + "\n", (err) => {
+                if (err) throw err;
+                console.log("Saved!");
+            });
+        }
+        //console.log(meatPrices);
+
     } catch (error) {
         console.error("Error fetching prices:", error);
     }
