9de369a2cdd57202f4eb17dfd114437097ab01e7
Samuel Hållman <samuel.hollman@hotmail.com>
2025-06-03 13:45:07 +0300

    Testing the db connection and creation!

--- db.js
+++ db.js
@@ -10,10 +10,22 @@ const con = mysql.createConnection({
   database: process.env.DATABASE,
 });
 
+const createTableQuery = `
+  CREATE TABLE IF NOT EXISTS products (
+    Itemid INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    ProductCategory VARCHAR(255) NOT NULL,
+    ProductName VARCHAR(255) NOT NULL,
+    ProductPrice VARCHAR(255) NOT NULL
+  );`;
+
+const insertTableQuery = `
+  INSET INTO products (
+  );`;
+
 con.connect((err) => {
   if (err) throw err;
   console.log("Connected to MySQL");
-  con.query("CREATE TABLE products (Itemid int AUTO_INCREMENT primary NOT NULL, ProductCategory varchar(255) NOT NULL, ProductName varchar(255) NOT NULL, ProductPrice varchar(255) NOT NULL);", (err, result) => {
+  con.query(createTableQuery, (err, result) => {
     if (err) throw err;
     console.log("Table created");
   });
