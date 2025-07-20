26bf8a4bab857f42f6fa26408f666563565ea1ac
Samuel Hållman <samuel.hollman@hotmail.com>
2025-07-20 22:27:25 +0300

    Links added to all categories

--- scraper.js
+++ scraper.js
@@ -2,47 +2,16 @@ import axios from "axios";
 import * as cheerio from "cheerio";
 import con from "./db.js";
 
+import { createRequire } from "node:module";
+
+const require = createRequire(import.meta.url);
+const links = require("./links.json");
+
+links.forEach(link => {
+  console.log(link.name, link.url);
+});
+
 const url = "https://www.s-kaupat.fi/tuotteet/liha-ja-kasviproteiinit-1/nauta";
-async function getAllCategoryUrls() {
-    try {
-        const response = await axios.get(categoryUrl, {
-                    headers: {
-                        "User-Agent": "Mozilla/5.0",
-                    },
-                });
-    
-        const $ = cheerio.load(response.data);
-
-        const categoryLinks = [];
-        
-        $('a[data-test-id="product-navigation-link"]').each((i, el) => {
-        const name = $(el).find(".menu-item--name").text().trim();
-        const href = $(el).attr("href");
-
-        if (name && href) {
-            categoryLinks.push({
-            name,
-            url: `https://www.s-kaupat.fi${href}`,
-            });
-        }
-        });
-
-        console.log(categoryLinks);
-    
-        // const categoryNamesSaved = [];
-    
-        // categoryNamesInMenu.each(() => {
-        //     const categoryName = $(this).find(".menu-item--name").text().trim();
-        //     if (categoryName) {
-        //         categoryNamesSaved.push({ categoryName });
-        //     }
-        // });
-    
-        // console.log("Category names:" + categoryNamesSaved);
-    } catch (err) {
-        console.error("Scraping failed:", err);
-    }
-}
 
 async function getAllPrices() {
     try {
